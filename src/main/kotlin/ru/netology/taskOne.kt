235bb7535fc1cc2f4959ad755a3c739c4bba9195
zkvKirov <zkkirov@yandex.ru>
2022-03-14 21:41:02 +0300

    change file "taskOne.kt"

--- src/main/kotlin/ru/netology/taskOne.kt
+++ src/main/kotlin/ru/netology/taskOne.kt
@@ -1,7 +1,7 @@
 package ru.netology
 
 fun main() {
-    val amount = 1_500_00
-    val commission = if (amount < 4_666_67) 35_00 else amount * 0.75 / 100
+    val amount = 5_000_00
+    val commission = if (amount * 0.75 / 100 < 35_00) 35_00 else amount * 0.75 / 100
     println("Комиссия за операцию = $commission копеек")
 }
